238efcbe15e3efe09258dc5d050dd8472f80fefc
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-05-09 23:44:48 +0300

    add front-end js code

---
+++ car-site-workshop/static/js/app.js
@@ -0,0 +1,12 @@
+document.getElementById("cars").addEventListener("click", (event) => {
+  if (event.target.classList.contains("more")) {
+    const desc = event.target.parentElement.querySelector(".description");
+    if (desc.style.display == "block") {
+      desc.style.display = "none";
+      event.target.textContent = "Show more details";
+    } else {
+      desc.style.display = "block";
+      event.target.textContent = "Hide details";
+    }
+  }
+});
