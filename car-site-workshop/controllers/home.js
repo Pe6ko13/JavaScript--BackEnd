e133aa122467c70199cbff5b130745fe45cd7dcb
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-05-26 09:56:46 +0300

    add notifications styles

--- car-site-workshop/controllers/home.js
+++ car-site-workshop/controllers/home.js
@@ -1,6 +1,11 @@
 module.exports = {
-  async home(req, res) {
-    const cars = await req.storage.getAll(req.query);
-    res.render("index", { cars, title: "Cars", query: req.query });
-  },
+    async home(req, res) {
+        const cars = await req.storage.getAll(req.query);
+        res.render('index', {
+            cars,
+            title: 'Cars',
+            query: req.query,
+            err: [{ msg: 'TEsT ErRoRrrrrr' }],
+        });
+    },
 };
